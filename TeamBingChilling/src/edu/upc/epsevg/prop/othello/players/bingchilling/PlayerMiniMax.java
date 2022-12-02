da0528ef75773c3c722b468545b7957c30761231
TorchZz <63902825+TorchZz@users.noreply.github.com>
2022-12-02 17:57:27 +0100

    Update PlayerMiniMax.java

--- TeamBingChilling/src/edu/upc/epsevg/prop/othello/players/bingchilling/PlayerMiniMax.java
+++ TeamBingChilling/src/edu/upc/epsevg/prop/othello/players/bingchilling/PlayerMiniMax.java
@@ -5,6 +5,7 @@
  */
 package edu.upc.epsevg.prop.othello.players.bingchilling;
 
+import edu.upc.epsevg.prop.othello.CellType;
 import edu.upc.epsevg.prop.othello.GameStatus;
 import edu.upc.epsevg.prop.othello.IAuto;
 import edu.upc.epsevg.prop.othello.IPlayer;
@@ -16,15 +17,17 @@ import java.util.Random;
 
 /**
  *
- * @author Walter
+ * @author Walter&Yisus
  */
 public class PlayerMiniMax implements IPlayer, IAuto {
 
     private String name;
     private GameStatus s;
+    private int profundidad;
 
-    public PlayerMiniMax(String name) {
+    public PlayerMiniMax(String name, int profundidad) {
         this.name = name;
+        this.profundidad = profundidad;
     }
 
     @Override
@@ -39,19 +42,31 @@ public class PlayerMiniMax implements IPlayer, IAuto {
      * @param s Tauler i estat actual de joc.
      * @return el moviment que fa el jugador.
      */
-    @Override
-    public Move move(GameStatus s) {
+    public Move move(GameStatus s, int profundidad) {
 
         ArrayList<Point> moves =  s.getMoves();
+        CellType currentp = s.getCurrentPlayer();
+        int max = Integer.MIN_VALUE;
+        Point millormov = null;
+        
         if(moves.isEmpty())
         {
             // no podem moure, el moviment (de tipus Point) es passa null.
             return new Move(null, 0L,0,  SearchType.RANDOM); 
         } else {
-            Random rand = new Random();
-            int q = rand.nextInt(moves.size());
-            return new Move( moves.get(q), 0L, 0, SearchType.RANDOM);         
+            for(int movi = 0; movi < moves.size(); ++movi){
+                GameStatus x = new GameStatus(s);
+                if(x.canMove(moves.get(movi), currentp)){
+                    x.movePiece(moves.get(movi));
+                    int actual = minimax(x, currentp, (profundidad - 1), Integer.MIN_VALUE, Integer.MAX_VALUE, false);
+                    if (actual > max) {
+                    max = actual;
+                    millormov = moves.get(movi);
+                    }
+                }
+            }         
         }
+        return new Move( millormov,0 , profundidad, SearchType.MINIMAX);
     }
 
     /**
@@ -61,6 +76,81 @@ public class PlayerMiniMax implements IPlayer, IAuto {
     @Override
     public String getName() {
         return "Helao(" + name + ")";
+    }
+    
+     public int minimax(GameStatus s, CellType currentp, int profunditat, int alpha, int beta, boolean esBendicion) {
+        CellType oponent = currentp;
+        int valor;
+        // Si la profundidad ya ha llegado a su limite no habra mas movimientos
+        // posibles, devolvemos la heurisica del tablero.
+        if (profunditat == 0 || s.currentPlayerCanMove() == false) {
+            return heuristica(s, currentp);
+        }
+        // Inicializamos variables en funcion si es min o max
+        if (esBendicion) { // Max
+            // Establecemos el valor inicial como al minimo asi cualquier valor sera
+            // superior.
+            valor = Integer.MIN_VALUE;
+        } else { // Min
+            // Establecemos el valor inicial como al maximo asi cualquier valor sera
+            // inferior.
+            valor = Integer.MAX_VALUE;
+            // Obtenemos el color del oponente que es el contrario al nuestro.
+            if (currentp.name() == "HELAO") {
+                oponent = "?";
+            } else {
+                oponent = "?";
+            }
+        }
+        // Para cada columna del tablero
+        ArrayList<Point> moves =  s.getMoves();
+        for (int movi = 0; movi < moves.size(); movi++) {
+            // Copia del tablero
+            GameStatus x = new GameStatus(s);
+            // Si se puede realizar un movimiento en la columna que estamos posicionados
+            if (x.canMove(moves.get(movi), currentp)) {
+                // Sumamos 1 a l numero de casos explorados
+
+                if (esBendicion) {
+                    // Realizamos el movimiento seleccionado con el color del jugador.
+                    x.movePiece(moves.get(movi));
+
+                    if (x.checkGameOver() && x.GetWinner() == currentp) {
+                        return GANADOR;
+                    }
+                    // Calculamos heuristica. Si esta es supererior al valor actual, substituimos
+                    // valor por esta.
+                    valor = Math.max(valor, minimax(x, currentp, profunditat - 1, alpha, beta, false));
+
+                    // Poda alfa-beta
+                    if (beta <= valor) {
+                        return valor;
+                    }
+
+                    alpha = Math.max(valor, alpha);
+                } else { // min
+                    // Realizamos el movimiento con el color del oponente ya que estamos en la capa
+                    // Min
+                    x.movePiece(moves.get(movi));
+
+                    if (tauler_aux.solucio(col, color_oponent)) {
+                        return PERDEDOR;
+                    }
+                    // Calculamos heuristica. Si esta es menor al valor actual substituimos por esta
+                    valor = Math.min(valor, minimax(tauler_aux, color, profunditat - 1, alpha, beta, true));
+
+                    // Realitzem la poda alpha-beta
+                    if (valor <= alpha) {
+                        return valor;
+                    }
+
+                    beta = Math.min(valor, beta);
+                }
+            }
+        }
+        // Devolvemos heuristica
+        return valor;
+
     }
 
     
